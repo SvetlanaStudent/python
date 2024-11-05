c1acee70c8d5da33257bda7478b148f1c00c41a2
SvetlanaStudent <sveta_0107@mail.ru>
2024-11-05 12:39:23 +0300

    Delete 05_lesson/lesson_5_task_5.py

--- 05_lesson/lesson_5_task_5.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-driver = webdriver.Firefox()
-driver.get("http://the-internet.herokuapp.com/inputs")
-input_field = driver.find_element(By.XPATH, "//input[@type='number']" )
-input_field.send_keys("1000")
-input_field.clear()
-input_field.send_keys("999")
-driver.quit()
