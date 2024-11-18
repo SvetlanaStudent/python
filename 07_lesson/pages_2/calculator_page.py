2e1e869ed9f53be2fc5e6cbeb94b553d1a26f779
SvetlanaStudent <sveta_0107@mail.ru>
2024-11-18 13:40:10 +0300

    Домашняя работа 7 - 2

--- 07_lesson/pages_2/calculator_page.py
+++ 07_lesson/pages_2/calculator_page.py
@@ -1,26 +1,3 @@
-# from selenium.webdriver.common.by import By
-#
-#
-# class CalculatorPage:
-#     def __init__(self, driver):
-#         self.driver = driver
-#         self.url = "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html"
-#
-#     def open(self):
-#         self.driver.get(self.url)
-#
-#     def set_delay(self, delay):
-#         delay_input = self.driver.find_element(By.CSS_SELECTOR, "#delay")
-#         delay_input.clear()
-#         delay_input.send_keys(delay)
-#
-#     def click_button(self, button_text):
-#         button = self.driver.find_element(By.XPATH, f"//button[text()='{button_text}']")
-#         button.click()
-#
-#     def get_result(self):
-#         return self.driver.find_element(By.CSS_SELECTOR, "#result").text
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -39,8 +16,11 @@ class CalculatorPage:
         delay_input.send_keys(delay)
 
     def click_button(self, button_text):
-        button = self.driver.find_element(By.XPATH, f"//button[text()='{button_text}']")
-        button.click()
+        buttons = self.driver.find_elements(By.CLASS_NAME, "btn")
+        for button in buttons:
+            if button.text == button_text:
+                button.click()
+                break
 
     def get_result(self):
         return WebDriverWait(self.driver, 50).until(
